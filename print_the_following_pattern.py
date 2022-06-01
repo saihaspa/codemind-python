a24657e84423f183e3ca5a874068068e5e457523
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-06-01 22:15:43 +0530

    rows=int(input()) k=0 for i in range(1, rows+1):     for space in range(1,(rows-i)+1):         print(end=' ')     while k!=(2*i-1):         print(i,end='')         k+= 1     k=0     print()

---
+++ print_the_following_pattern.py
@@ -0,0 +1,10 @@
+rows=int(input())
+k=0
+for i in range(1, rows+1):
+    for space in range(1,(rows-i)+1):
+        print(end=' ')
+    while k!=(2*i-1):
+        print(i,end='')
+        k+= 1
+    k=0
+    print()
