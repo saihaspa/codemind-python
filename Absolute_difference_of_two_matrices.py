994464641589275eb11160edde4618b6445c4396
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-06-05 19:05:28 +0530

    #import math r=int(input()) a=[list(map(int,input().split())) for i in range(r)] b=[list(map(int,input().split())) for i in range(r)]
    for i in range(r):
        s=[]
        for j in range(r):
            s.append(abs(a[i][j]-b[i][j]))
        print(*s)

---
+++ Absolute_difference_of_two_matrices.py
@@ -0,0 +1,12 @@
+#import math
+r=int(input())
+a=[list(map(int,input().split())) for i in range(r)]
+b=[list(map(int,input().split())) for i in range(r)]
+
+for i in range(r):
+    s=[]
+    for j in range(r):
+        s.append(abs(a[i][j]-b[i][j]))
+    print(*s)
+
+    
