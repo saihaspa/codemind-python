0ef829dfd7aa50ec05024ae8498c0c9816e9a35e
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-06-07 10:31:59 +0530

    def sum_digits_string(str1):     sum_digit = 0     for x in str1:         if x.isdigit() == True:             z = int(x)             sum_digit = sum_digit + z
        return sum_digit
    
    a=input()   
    print(sum_digits_string(a))

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.py
@@ -0,0 +1,11 @@
+def sum_digits_string(str1):
+    sum_digit = 0
+    for x in str1:
+        if x.isdigit() == True:
+            z = int(x)
+            sum_digit = sum_digit + z
+
+    return sum_digit
+
+a=input()   
+print(sum_digits_string(a))
