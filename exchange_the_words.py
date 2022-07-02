272411229d3e9150624332fb27f665bf710a1772
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-07-02 06:44:19 +0530

    s=input() w=s.split(" ") n_w=w[::-1] n_s=" ".join(n_w) print(n_s) sen=" ".join(r_s) print(sen)

--- exchange_the_words.py
+++ exchange_the_words.py
@@ -1,7 +1,7 @@
-def reverseWordSentence(Sentence):
-    words = Sentence.split(" ")
-    newWords = words[::-1]
-    newSentence = " ".join(newWords)
-    return newSentence
-Sentence =input()
-print(reverseWordSentence(Sentence))
+s=input()
+w=s.split(" ")
+n_w=w[::-1]
+n_s=" ".join(n_w)
+print(n_s)
+sen=" ".join(r_s)
+print(sen)
