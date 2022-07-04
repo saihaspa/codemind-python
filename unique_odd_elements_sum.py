5f54322e542fa63e07165fa0313918b94f153f8b
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-07-04 06:15:10 +0530

    n=int(input()) a=list(map(int,input().split())) b=set(a) s=0 for i in b:     if i%2!=0:         s=s+i print(s)

--- unique_odd_elements_sum.py
+++ unique_odd_elements_sum.py
@@ -1,10 +1,8 @@
 n=int(input())
 a=list(map(int,input().split()))
-b=[]
 b=set(a)
-c=list(b)
 s=0
-for i in range(len(c)):
-    if c[i]%2!=0:
-        s=s+c[i]
+for i in b:
+    if i%2!=0:
+        s=s+i
 print(s)
