5048970aa897eb9800c43f33b147a06698ffc777
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-07-05 19:35:54 +0530

    s=input() s=s.lower() t=s[::-1] if(t==s):     print('True') else:     print('False')

--- Palindrome_or_not.py
+++ Palindrome_or_not.py
@@ -1,8 +1,7 @@
-a_string =input()
-def palindrome(string):
-    string = string.lower()
-    return string == string[::-1]
-if(palindrome(a_string)):
+s=input()
+s=s.lower()
+t=s[::-1]
+if(t==s):
     print('True')
 else:
     print('False')
