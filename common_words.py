f4f269937b631153452a78ed13130761611c7b96
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-07-12 22:46:10 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() d=[] c=0 for i in s2.split():     if i in s1.split():         d.append(i)         c+=1 if(c==0):     print(-1) else:     print(*d)

--- common_words.py
+++ common_words.py
@@ -1,12 +1,15 @@
-s1=input().lower()
-s2=input().lower()
-if len(s1)>len(s2):
-    for i in s1.split():
-        for j in s2.split():
-            if i==j:
-                print(i,end=' ')
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+d=[]
+c=0
+for i in s2.split():
+    if i in s1.split():
+        d.append(i)
+        c+=1
+if(c==0):
+    print(-1)
 else:
-    for i in s2.split():
-        for j in s1.split():
-            if i==j:
-                print(i,end=' ')
+    print(*d)
+        
