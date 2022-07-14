3aa675f1746bf0cd832dba054cdd4176ba6c2a8a
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-07-14 07:20:59 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() a=[] for i in s1:     if i not in s2:         if i==" ":             continue         if i not in a:             a.append(i) for i in s2:     if i not in s1:         if i==" ":             continue         if i not in a:             a.append(i) a.sort() for i in a:     print(i,end='')

--- non_common_characters.py
+++ non_common_characters.py
@@ -3,21 +3,18 @@ s2=input()
 s1=s1.lower()
 s2=s2.lower()
 a=[]
-d=[]
 for i in s1:
     if i not in s2:
         if i==" ":
             continue
-        if ord(i) not in a:
-            a.append(ord(i))
+        if i not in a:
+            a.append(i)
 for i in s2:
     if i not in s1:
         if i==" ":
             continue
-        if ord(i) not in a:
-            a.append(ord(i))
+        if i not in a:
+            a.append(i)
 a.sort() 
 for i in a:
-    print(chr(i),end='')
-        
-    
+    print(i,end='')
