c2594162b61c28982e2f97a519393578d3ede1b1
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-07-19 07:12:21 +0530

    def maxRepeating(str):
        l = len(str)
        count = 0
        res = str[0]
        for i in range(l):
             
            cur_count = 1
            for j in range(i + 1, l):
         
                if (str[i] != str[j]):
                    break
                cur_count += 1
            if cur_count > count :
                count = cur_count
                res = str[i]
        return count
    str = input()
    print(maxRepeating(str))

---
+++ Consecutive_Characters.py
@@ -0,0 +1,19 @@
+def maxRepeating(str):
+ 
+    l = len(str)
+    count = 0
+    res = str[0]
+    for i in range(l):
+         
+        cur_count = 1
+        for j in range(i + 1, l):
+     
+            if (str[i] != str[j]):
+                break
+            cur_count += 1
+        if cur_count > count :
+            count = cur_count
+            res = str[i]
+    return count
+str = input()
+print(maxRepeating(str))
