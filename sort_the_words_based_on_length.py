cb8e65a790b878f2ab1a81656cdd0e58cb6995c2
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-10-05 15:19:29 +0530

    a=input().split() a=sorted(a) a.sort(key=len) print(*a)

--- sort_the_words_based_on_length.py
+++ sort_the_words_based_on_length.py
@@ -1,15 +1,4 @@
-s=input()
-t=s.split()
-n=len(t)
-for i in range(n):
-    for j in range(i+1,n):
-        if(len(t[i])>len(t[j])):
-            temp=t[i]
-            t[i]=t[j]
-            t[j]=temp
-        if(len(t[i])==len(t[j])):
-            t.sort()
-        for k in t:
-            print(k,end=' ')
-        break
-    break
+a=input().split()
+a=sorted(a)
+a.sort(key=len)
+print(*a)
