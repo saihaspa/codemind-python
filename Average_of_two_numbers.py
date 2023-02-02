22d2327719e934a603d4b84a720286ea2a835537
saihaspa <97425320+saihaspa@users.noreply.github.com>
2023-02-02 12:43:26 +0530

    a,b=map(int,input().split()) c=(a+b)/2 print("Average of",end=' ') print("%d"%a,end=' ') print("and",end=' ') print("%d"%b,end=' ') print("is:",end=' ') print("%.2f"%c,end=' ')

---
+++ Average_of_two_numbers.py
@@ -0,0 +1,8 @@
+a,b=map(int,input().split())
+c=(a+b)/2
+print("Average of",end=' ')
+print("%d"%a,end=' ')
+print("and",end=' ')
+print("%d"%b,end=' ')
+print("is:",end=' ')
+print("%.2f"%c,end=' ')
